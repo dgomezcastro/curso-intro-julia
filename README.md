0907b205301c7832f9690a9f5dd4115e3f523405
David Gómez-Castro <david@gomezcastro.xyz>
2025-02-14 19:42:33 +0100

    first working prototype

---
+++ README.md
@@ -0,0 +1,4 @@
+# Curso de introducción a Julia
+David Gómez-Castro (UAM)
+Marzo 2025. Universidad Complutense de Madrid
+
---
+++ src/_post-render.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+src_folder = "."
+
+types = ["slides", "html"]
+
+for type in types:
+    dest_folder = os.path.join("..", f"{type}")
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    for filename in os.listdir(src_folder):
+        if filename.endswith(f"-{type}.html"):
+            dest_file = os.path.join(dest_folder, filename)
+            if os.path.exists(dest_file):
+                os.remove(dest_file)
+            shutil.move(os.path.join(src_folder, filename), dest_folder)
